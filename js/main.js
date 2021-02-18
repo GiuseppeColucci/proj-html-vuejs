a0318396c009303a49cc52f7648ef67c55ae67c5
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-18 15:13:21 +0100

    prova per img

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-const { vue } = require("laravel-mix");
+// const { vue } = require("laravel-mix");
 
 new Vue({
     el:'#app',
     data:{
-
-    }
+        linkNav:[
+            'Home',
+            'Who We Are',
+            'What We Do',
+            'Where We Work',
+            'Careers',
+            'APPLY',
+            'News',
+        ],
+        icoMain:["fas fa-road","fas fa-video","fas fa-tint"],
+    },
 })//fine vue
+console.log('tutto ok')
