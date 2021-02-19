9907904b1074ef08fa7952050a8827fe587e10d9
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-02-19 09:44:39 +0100

    box main 1 completato

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 new Vue({
     el:'#app',
     data:{
+        iconLogoNav:'logo_seo_w_1x',
         linkNav:[
             'Home',
             'Who We Are',
@@ -12,7 +13,23 @@ new Vue({
             'APPLY',
             'News',
         ],
-        icoMain:["fas fa-road","fas fa-video","fas fa-tint"],
+        mainMiniBox:[
+            {
+                icon:'road',
+                title:'Establish Goals',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis at impedit ipsa dolor accusamus!',
+            },
+            {
+                icon:'fas fa-video',
+                title:'Work With a Team',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis at impedit ipsa dolor accusamus!',
+            },
+            {
+                icon:'fas fa-tint',
+                title:'Get Result',
+                text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis at impedit ipsa dolor accusamus!',
+            }
+        ]
     },
 })//fine vue
 console.log('tutto ok')
